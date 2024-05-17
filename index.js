69e460c5d4fcd00794c2f52c14a256e18e097b8e
ftygshu <ivichita2psg@hotmail.com>
2024-05-17 07:02:52 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const pullAtValue = (arr, pullArr) => {
-  let removed = [],
-    pushToRemove = arr.forEach((v, i) =>
-      pullArr.includes(v) ? removed.push(v) : v,
-    ),
-    mutateTo = arr.filter((v, i) => !pullArr.includes(v));
-  arr.length = 0;
-  mutateTo.forEach((v) => arr.push(v));
-  return removed;
-};
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
